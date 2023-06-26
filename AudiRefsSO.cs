e6108b9dfba435cfdd3db77cbf8719aacaccb81e
theApril94 <115942743+theApril94@users.noreply.github.com>
2023-06-26 19:14:50 +0200

    Add files via upload

---
+++ AudiRefsSO.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu()]
+public class AudiRefsSO : ScriptableObject
+{
+
+    public AudioClip[] chop;
+    public AudioClip[] deliveryFail;
+    public AudioClip[] deliverySucces;
+    public AudioClip[] footstep;
+    public AudioClip[] objectDrop;
+    public AudioClip[] objectPickUp;
+    public AudioClip stoveSizzle;
+    public AudioClip[] trash;
+    public AudioClip[] warning;
+    
+
+
+}
---
+++ RecipeSO.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu()]
+public class RecipeSO :ScriptableObject
+{
+
+
+    public List<KitchenObjectSO> kitchenObjectSOList;
+    public string recipeName;
+
+
+
+}
